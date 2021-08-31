b6680a02131a9fbdc51e05ec72b5280b84f1131b
pete <petr.krasnoshchekov@wirenboard.ru>
2021-08-31 11:48:22 +0500

    Properties list translation

--- src/editors/object.js
+++ src/editors/object.js
@@ -900,7 +900,11 @@ export class ObjectEditor extends AbstractEditor {
     const checkbox = this.theme.getCheckbox()
     checkbox.style.width = 'auto'
 
-    if (this.schema.properties[key] && this.schema.properties[key].title) { labelText = this.schema.properties[key].title } else { labelText = key }
+    if (this.schema.properties[key] && this.schema.properties[key].title) {
+      labelText = this.translateProperty(this.schema.properties[key].title)
+    } else {
+      labelText = key
+    }
 
     const label = this.theme.getCheckboxLabel(labelText)
 
