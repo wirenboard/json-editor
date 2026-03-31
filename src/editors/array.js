ce83311e311a4cf7b4cb88d953eaf87d1d9a5582
Валерий <trofimof09@mail.ru>
2026-03-31 16:41:13 +0300

    feat: Update delete and copy buttons INT-927

--- src/editors/array.js
+++ src/editors/array.js
@@ -529,7 +529,7 @@ export class ArrayEditor extends AbstractEditor {
   }
 
   _createDeleteButton (i, holder) {
-    const button = this.getButton(this.getItemTitle(), 'delete', 'button_delete_row_title', [this.getItemTitle()])
+    const button = this.getButton('button_delete_row_title_short', 'delete', 'button_delete_row_title', [this.getItemTitle()])
     button.classList.add('delete', 'json-editor-btntype-delete')
     button.setAttribute('data-i', i)
     button.addEventListener('click', e => {
@@ -571,7 +571,7 @@ export class ArrayEditor extends AbstractEditor {
   }
 
   _createCopyButton (i, holder) {
-    const button = this.getButton(this.getItemTitle(), 'copy', 'button_copy_row_title', [this.getItemTitle()])
+    const button = this.getButton('button_copy_row_title_short', 'copy', 'button_copy_row_title', [this.getItemTitle()])
     const schema = this.schema
     button.classList.add('copy', 'json-editor-btntype-copy')
     button.setAttribute('data-i', i)
